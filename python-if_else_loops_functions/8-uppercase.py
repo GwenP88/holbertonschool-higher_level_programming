7e599d6f5e1e43376149441fd966beaeb9cc01f1
GwenP88 <12777@holbertonstudents.com>
2026-01-14 17:07:08 +0100

    Task 8 execution: 8-uppercase.py - modification error code

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -15,6 +15,7 @@ def uppercase(str):
         if code >= 97 and code <= 122:
             new_code = code - 32
             new_c = chr(new_code)
-        new_string += new_c      
+        new_string += new_c
+              
     print("{}".format(new_string), end="")
     print()

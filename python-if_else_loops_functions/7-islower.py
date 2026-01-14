843b9b2161bba923509f61183d30447e95d8728c
GwenP88 <12777@holbertonstudents.com>
2026-01-14 16:23:58 +0100

    Task 7 execution: 7-islower.py

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def islower(c):
+    """
+    checks for lowercase character
+
+    Params:
+        c (str): a letter
+    Returns:
+        True if c is lowercase
+        False otherwise
+    """
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False

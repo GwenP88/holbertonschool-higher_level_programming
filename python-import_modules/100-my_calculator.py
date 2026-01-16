c1e7ec03e5cf0d455f85c9603aa1af5a622ae26b
GwenP88 <12777@holbertonstudents.com>
2026-01-16 12:50:35 +0100

    task advanced 6 - 100-my_calculator.py - modif code (exit and operator)

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,21 +5,19 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print("1")
-        exit(1)
+        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operators = ["+", "-", "*", "/"]
     operator = sys.argv[2]
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
-        print("1")
-        exit(1)
-    if operator == operators[0]:
+        sys.exit(1)
+    if operator == "+":
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-    elif operator == operators[1]:
+    elif operator == "-":
         print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif operator == operators[2]:
+    elif operator == "*":
         print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
     else:
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))

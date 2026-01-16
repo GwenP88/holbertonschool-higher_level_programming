60425b1a79ea129ba928a5c30ce2da5e59b4c3df
GwenP88 <12777@holbertonstudents.com>
2026-01-16 13:25:19 +0100

    task advanced 6 - 100-my_calculator.py - version 7

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -4,11 +4,11 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <a> <operator> <b>".format("./100-my_calculator.py"))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    operator = sys.argv[2]
-    if operator not in "+-*/":
-        print("Unknown operator. Available operators: {} and /".format("+, -, *"))
+    operator = {'+', '-', '*', '/'}
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])

21f9bcad70a7ab643ec8afff68f39a982a35cd29
GwenP88 <12777@holbertonstudents.com>
2026-01-16 12:39:33 +0100

    task advanced 6 - 100-my_calculator.py - imports all functions from the file calculator_1.py and handles basic operations.

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == "__main__":
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operators = ["+", "-", "*", "/"]
+    operator = sys.argv[2]
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    if operator == operators[0]:
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif operator == operators[1]:
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    elif operator == operators[2]:
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+    else:
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))

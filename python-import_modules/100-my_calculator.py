1dfc2d428ba991dc5932fae026d51fdb973e0381
GwenP88 <12777@holbertonstudents.com>
2026-01-16 13:26:07 +0100

    task advanced 6 - 100-my_calculator.py - version 7

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     operator = {'+', '-', '*', '/'}
-    if sys.argv[2] not in operator:
+    if argv[2] not in operator:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
     if operator == "+":
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == "-":

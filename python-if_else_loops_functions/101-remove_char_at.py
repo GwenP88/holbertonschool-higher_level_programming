3ff655efa9f2f5fefc57fe835086f178c34acc3f
GwenP88 <12777@holbertonstudents.com>
2026-01-16 12:11:16 +0100

    task advanced - 101-remove_char_at.py - management if n less than 0

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     if n < 0:
-        n = - n
+        return str
+    if n >= len(str):
+        return str
     new_str = str[:n] + str[n+1:]
     return new_str

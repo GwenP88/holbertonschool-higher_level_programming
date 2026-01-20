0614c5f1745ad44336868efd76d2d0ca01b95998
GwenP88 <12777@holbertonstudents.com>
2026-01-20 11:58:00 +0100

    task 3 - 3-common_elements.py - function that returns a set of common elements in two sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    value_set = set()
+    for value in set_1:
+        if value in set_2:
+            value_set.add(value)
+    return value_set

e23ee1c554261bf145bd8b3cac844b99c3699628
GwenP88 <12777@holbertonstudents.com>
2026-01-20 16:47:30 +0100

    modif code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for i in matrix:
+    for values in matrix:
         row = []
-        for j in i:
-            square = j ** 2
+        for value in values:
+            square = value ** 2
             row.append(square)
         new_matrix.append(row)
     return new_matrix
--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    """
     value_set = set()
     for value in set_1:
         if value in set_2:
             value_set.add(value)
     return value_set
+    """
+    return set_1 & set_2

48abb97ebb9339b0a53faa1fb6f0d4bfcbece261
GwenP88 <12777@holbertonstudents.com>
2026-01-28 15:46:18 +0100

    task 0: add docstring

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+"""
 
 class Rectangle:
+    """
+    Rectangle class that defines a rectangle.
+    """
 
     def __init__(self):
         pass

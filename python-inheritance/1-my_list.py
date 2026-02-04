1c897a7e199cbc4cb7a60962056860b8105e39e0
GwenP88 <12777@holbertonstudents.com>
2026-02-04 08:47:12 +0100

    modifying docstrings and removing a space

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 """
-This module defines a custom list class that can print its elements in
-sorted order.
+This module defines a custom list class
 """
 
 
-class MyList (list):
+class MyList(list):
     """
-    MyList - Defines a custom list object.
-
     This class inherits from list and adds a method to print the list sorted.
     """
 
     def print_sorted(self):
         """
         print_sorted - Prints the list elements in ascending sorted order
-
-        Return: None
         """
 
         print(sorted(self))

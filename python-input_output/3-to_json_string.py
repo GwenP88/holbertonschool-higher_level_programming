067e6d1b5307a3a6d7c421575734808ec236c74f
GwenP88 <12777@holbertonstudents.com>
2026-02-10 14:47:11 +0100

    Implement to_json_string using json.dumps

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import json
+"""Convert objects to JSON strings."""
+
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of an object."""
+    return json.dumps(my_obj)

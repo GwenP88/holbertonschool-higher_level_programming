e4cff661a6b95699c6b92e9217c116e167dc04d5
GwenP88 <12777@holbertonstudents.com>
2026-02-10 15:18:10 +0100

    Implement load_from_json_file to load objects from JSON

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Load Python objects from JSON files."""
+import json
+
+
+def load_from_json_file(filename):
+    """Return the Python object represented by a JSON file."""
+    with open(filename, 'r', encoding='utf-8') as fichier:
+        return json.load(fichier)

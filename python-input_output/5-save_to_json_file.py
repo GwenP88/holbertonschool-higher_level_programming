6e32e6d9378c11df59c974139946c5c208dd295b
GwenP88 <12777@holbertonstudents.com>
2026-02-10 15:04:21 +0100

    Implement save_to_json_file to write objects as JSON

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Save a Python object to a file in JSON format."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write a JSON representation of an object to a file."""
+    with open(filename, 'w', encoding="utf-8") as fichier:
+        fichier.write(json.dumps(my_obj))

69313baca12848b9799210e92cce73875473cd84
GwenP88 <12777@holbertonstudents.com>
2026-02-10 14:57:31 +0100

    Implement from_json_string using json.loads

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Convert JSON strings to objects."""
+import json
+
+
+def from_json_string(my_str):
+    """Return an object representation of a JSON string."""
+    return json.loads(my_str)

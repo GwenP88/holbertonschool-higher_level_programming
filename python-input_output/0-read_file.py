ab31631dc68525d715210193fc568664921ee6a6
GwenP88 <12777@holbertonstudents.com>
2026-02-10 14:04:05 +0100

    add docstring on file 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""File reading utility."""
+
 
 def read_file(filename=""):
+    """Read a UTF-8 text file and print it."""
     with open(filename, 'r', encoding="utf-8") as fichier:
         print("{}".format(fichier.read()), end="")

d647cbadc15b8357ffbeff62d8ee1a4beb6cf105
GwenP88 <12777@holbertonstudents.com>
2026-02-10 17:16:50 +0100

    Fix pascal_triangle to build correct Pascal's triangle rows

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Generate Pascal's triangle of n as a list of lists"""
+
+
+def pascal_triangle(n):
+    """Return Pascal's triangle of n"""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_line = triangle[-1]
+        new_line = [1]
+        for i in range(1, len(prev_line)):
+            new_line.append(prev_line[i - 1] + prev_line[i])
+        new_line.append(1)
+        triangle.append(new_line)
+    return triangle

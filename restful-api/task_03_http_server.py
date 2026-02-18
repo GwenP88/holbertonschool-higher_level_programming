27ddcef5617103c581a0508ff9be39a8674ff5ba
GwenP88 <12777@holbertonstudents.com>
2026-02-18 10:48:30 +0100

    add Fix Content-Type headers to match checker expectations

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,27 +16,27 @@ class MyHandler(BaseHTTPRequestHandler):
         """
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
         elif self.path == "/data":
             self.send_response(200)
-            data = {"name": "John", "age": 30, "city": "New York"}
+            data = {"name": "John", "age": 30, "city": "New York\n"}
             json_dict = json.dumps(data)
-            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json_dict.encode("utf-8"))
         elif self.path == "/info":
             self.send_response(200)
             data = {"version": "1.0",
-                    "description": "A simple API built with http.server"}
+                    "description": "A simple API built with http.server\n"}
             json_dict = json.dumps(data)
-            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json_dict.encode("utf-8"))
         else:

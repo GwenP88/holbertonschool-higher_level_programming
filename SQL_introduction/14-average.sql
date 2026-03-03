8d91f1da60857bae95a037e483998a6d2c289805
GwenP88 <12777@holbertonstudents.com>
2026-03-03 19:55:54 +0100

    SQL_introduction: add script to compute average score

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Computes the score average of all records.
+SELECT AVG (score) FROM second_table;

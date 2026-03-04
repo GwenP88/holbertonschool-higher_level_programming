8ca3221b8880fb8941d7ba6e123e44c8afd28e88
GwenP88 <12777@holbertonstudents.com>
2026-03-04 14:46:11 +0100

    Add SQL script to create unique_id table with NOT NULL id and default value 1 and unique

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- Script that creates the table unique_id on your MySQL server.
+-- If the table already exists, script should not fail.
+-- Create the table unique_id (id INT with default value 1 and must be unique, name VARCHAR (256)).
+CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL DEFAULT 1 UNIQUE, name VARCHAR(256));

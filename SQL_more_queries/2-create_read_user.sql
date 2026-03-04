ee0c4fda8acbce124f98cea63a6b8d7a9369e1cd
GwenP88 <12777@holbertonstudents.com>
2026-03-04 12:09:03 +0100

    task 2, fix sql script only privilege on database hbtn_0d_2 for user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create the user user_0d_2 (password should be set to user_0d_2_pwd)
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- Grant all privileges on the MySQL server to user_0d_2
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+-- Grant select privileges on the MySQL server to user_0d_2
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';

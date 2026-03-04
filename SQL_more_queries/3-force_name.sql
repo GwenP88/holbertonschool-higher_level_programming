57502cc9fafa0c3131964685b308521b500ff1cc
GwenP88 <12777@holbertonstudents.com>
2026-03-04 14:36:46 +0100

    Fix SQL script to create table force_name without hardcoding database name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,4 @@
 -- Script that creates the table force_name on your MySQL server.
--- If user or database amready exists, script should not fail.
--- Select the database hbtn_0d_2
-USE hbtn_0d_2;
+-- If user or database already exists, script should not fail.
 -- Create the table force_name (id INT, name VARCHAR (256) can't be null)
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);

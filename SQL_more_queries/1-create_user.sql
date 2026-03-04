5a9a24e7dde907901ba7042d5ccd5323da38438c
GwenP88 <12777@holbertonstudents.com>
2026-03-04 11:52:48 +0100

    Add SQL script to create MySQL user user_0d_1 with password user_0d_1_pwd and all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that creates the MySQL server user user_0d_1.
+-- The user user_0d_1 should have all privileges and the password should be set to user_0d_1_pwd.
+-- Create the MySQL user user_0d_1 if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 

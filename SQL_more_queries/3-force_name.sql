c514e0dee2293d4e742a7352eae5c75e8e861c19
GwenP88 <12777@holbertonstudents.com>
2026-03-04 14:34:30 +0100

    Add SQL script to create table force_name with name set to NOT NULL in database hbtn_0d_2

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,6 @@
 -- Script that creates the table force_name on your MySQL server.
 -- If user or database amready exists, script should not fail.
+-- Select the database hbtn_0d_2
+USE hbtn_0d_2;
 -- Create the table force_name (id INT, name VARCHAR (256) can't be null)
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);

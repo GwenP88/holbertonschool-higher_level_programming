8a5f72859613e70848f17b59a7c77f71dee1763b
GwenP88 <12777@holbertonstudents.com>
2026-03-06 11:59:41 +0100

    feat: filter states by user input using format

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Connects to a MySQL database
+and lists all states starting with N, ordered by id."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=user,
+        passwd=password,
+        db=database,
+        charset="utf8"
+        )
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC".format(searched)
+        )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()

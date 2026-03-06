18520b72dc4cb2fa60c6a9676c787ff4fc35c1bc
GwenP88 <12777@holbertonstudents.com>
2026-03-06 16:58:02 +0100

    add else after if......

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
 
     state = session.query(State).order_by(State.id.asc()).first()
     if state is None:
-        print("Nothing")      
-    print(f"{state.id}: {state.name}")
+        print("Nothing")
+    else:
+        print(f"{state.id}: {state.name}")
     session.close()

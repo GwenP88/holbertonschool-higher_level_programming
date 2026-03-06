5bdb015fc5ce556eb6271f123f5c113c99ba4859
GwenP88 <12777@holbertonstudents.com>
2026-03-06 11:49:19 +0100

    fix: make N filter case-sensitive in SQL query

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states " \
-        "WHERE BINARY LEFT(name, 1) = 'N' " \
+        "SELECT * FROM states "
+        "WHERE BINARY LEFT(name, 1) = 'N' "
         "ORDER BY id ASC"
         )
     query_rows = cur.fetchall()

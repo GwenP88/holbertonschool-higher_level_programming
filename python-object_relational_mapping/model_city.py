f76febeb9f62c6fd38b046c97a84b4f5eb8d67c8
GwenP88 <12777@holbertonstudents.com>
2026-03-06 20:11:17 +0100

    Add script to list cities with their state names

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines the City class
+and the declarative base."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+class City(Base):
+    """City class that maps to the cities table."""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

e1319f3994b6df96231273fde28478ea15d868c5
GwenP88 <12777@holbertonstudents.com>
2026-03-06 11:43:33 +0100

    fix pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,9 @@ if __name__ == "__main__":
         )
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE LEFT(name, 1) = 'N' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE LEFT(name, 1) = 'N' ORDER BY id ASC"
+        )
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)

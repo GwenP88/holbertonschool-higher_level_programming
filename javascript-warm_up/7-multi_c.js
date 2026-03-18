f735bf2faa8b7ca391484f2f3f0c3a6fc21232dc
GwenP88 <12777@holbertonstudents.com>
2026-03-18 08:18:43 +0100

    update code for semistandard validation

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const arg2 = parseInt(process.argv[2]);
 if (isNaN(arg2)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < arg2; i++){
+  for (let i = 0; i < arg2; i++) {
     console.log('C is fun');
-  }  
+  }
 }

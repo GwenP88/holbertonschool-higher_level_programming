1d8c7d05904c11621764afed1b8f1b3394661709
GwenP88 <12777@holbertonstudents.com>
2026-03-23 14:14:45 +0100

    JavaScript script that updates the text of the header element to New Headerss! when the user clicks on the element with id update_header

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,6 +1,6 @@
 const ul = document.querySelector('.my_list');
 const addItem = document.querySelector('#add_item');
-addItem.addEventListener('click', function() {
+addItem.addEventListener('click', function () {
   const newLi = document.createElement('li');
   newLi.textContent = 'Item';
   ul.appendChild(newLi);
---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('header');
+const updateHeader = document.getElementById('update_header');
+updateHeader.addEventListener('click', function () {
+  header.textContent = 'New Header!!!';
+});

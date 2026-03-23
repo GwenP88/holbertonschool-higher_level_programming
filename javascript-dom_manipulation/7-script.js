0a1605e25d94b94830ae19a59d7e05abad6f62e5
GwenP88 <12777@holbertonstudents.com>
2026-03-23 14:44:09 +0100

    JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json. All movie titles must be list in the HTML ul element with id list_movies.

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,10 @@
+const listMovies = document.querySelector('#list_movies');
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(film => {
+      const li = document.createElement('li');
+      li.textContent = film.title;
+      listMovies.appendChild(li);
+    });
+  });

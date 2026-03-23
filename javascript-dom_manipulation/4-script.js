0abe02987982ae63148c01ba7a68056d04ddd25c
GwenP88 <12777@holbertonstudents.com>
2026-03-23 14:07:29 +0100

    JavaScript script that adds a li element to a list when the user clicks on the element with id add_item

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector('.my_list');
+const addItem = document.querySelector('#add_item');
+addItem.addEventListener('click', function() {
+  const newLi = document.createElement('li');
+  newLi.textContent = 'Item';
+  ul.appendChild(newLi);
+});
